8281bcda31122e60cc43cc48a90fa4a486755fc2
IamFinn7 <cpn2003.py@gmail.com>
2023-11-04 12:44:43 +0700

    SearchController

--- src/java/com/Controllers/SearchController.java
+++ src/java/com/Controllers/SearchController.java
@@ -1,7 +1,9 @@
 package com.Controllers;
 
+import com.Utilities.SearchDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,9 +18,24 @@ public class SearchController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String searchTerm = request.getParameter("search");
-        PrintWriter out = response.getWriter();
-        out.print(searchTerm);
+//        String searchTerm = request.getParameter("search");
+//        PrintWriter out = response.getWriter();
+//        out.print(searchTerm);
+
+        try {
+            String txtSearch = request.getParameter("txtSearch");
+//            SearchDAO searchDao = new SearchDAO();
+//            List<String> userName = searchDao.searchUser(txtSearch);
+//            for (String name : userName) {
+//                System.out.println(name);
+//            } 
+            SearchDAO searchDao = new SearchDAO();
+            int count = searchDao.count(txtSearch);
+            System.out.println(count);
+            request.getRequestDispatcher("testSearch/index.jsp").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
